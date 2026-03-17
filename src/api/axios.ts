a92f72c58b8d77d1b5ac4132f16dd958db4bbd32
Siddharth Sharma <siddharth54330@gmail.com>
2026-03-17 11:55:26 +0530

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    withCredentials: true,   // sends httpOnly cookie on every request
+    withCredentials: false,   // sends httpOnly cookie on every request
     timeout: 15000,
 });
 
